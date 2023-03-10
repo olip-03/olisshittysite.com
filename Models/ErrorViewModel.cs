89b7031ad76747e7d26dda3a2b9a14f3c9634c40
Oliver Posa <OPosa@monadel.com.au>
2023-03-10 09:29:20 +0800

    Default project created
    Created the default MVC project which this site will be based off of.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace olisshittysite.com.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
